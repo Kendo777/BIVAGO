3a741311d20e62b61b825047e60ce0f90a569356
Jorge Ruiz <jorgerv97@gmail.com>
2021-01-18 16:03:03 +0100

    Jsons merge done

--- metaSearch.php
+++ metaSearch.php
@@ -8,21 +8,27 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
 if(isset($_GET['search'])) {
 
-	$json = file_get_contents("http://localhost/PAPI/OnlineShop/getProducts.php?user=CrapiKodaa&psw=12345&search=".$_GET["search"]);
+	$json1 = file_get_contents("http://localhost/PAPI/OnlineShop/getProducts.php?user=CrapiKodaa&psw=12345&search=".$_GET["search"]);
+	$json2 = file_get_contents("http://localhost/PAPI/IA-II/getProducts.php?user=CrapiKodaa&psw=12345&search=".$_GET["search"]);
+	$json = json_encode(array_merge(json_decode($json1, true),json_decode($json2, true)));
 	//var_dump($data);
 	header('Content-type: application/json');
 	echo $json;
 }
 else if(isset($_GET['filter']))
 {
-	//$json = file_get_contents("https://apimlozanoo20.000webhostapp.com/OnlineShop/getProducts.php?user=CrapiKodaa&psw=12345&search=".$_GET["search"]);
+	$json1 = file_get_contents("http://localhost/PAPI/OnlineShop/getProducts.php?user=CrapiKodaa&psw=12345&filter=".$_GET["filter"]);
+	$json2 = file_get_contents("http://localhost/PAPI/IA-II/getProducts.php?user=CrapiKodaa&psw=12345&filter=".$_GET["filter"]);
+	$json = json_encode(array_merge(json_decode($json1, true),json_decode($json2, true)));
 	//var_dump($data);
 	header('Content-type: application/json');
 	echo $json;
 }
 else
 {
-	$json = file_get_contents("http://localhost/PAPI/OnlineShop/getProducts.php?user=CrapiKodaa&psw=12345");
+	$json1 = file_get_contents("http://localhost/PAPI/OnlineShop/getProducts.php?user=CrapiKodaa&psw=12345");
+	$json2 = file_get_contents("http://localhost/PAPI/IA-II/getProducts.php?user=CrapiKodaa&psw=12345");
+	$json = json_encode(array_merge(json_decode($json1, true),json_decode($json2, true)));
 	//var_dump($data);
 	header('Content-type: application/json');
 	echo $json;
